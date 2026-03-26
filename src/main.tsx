c6459b0108de27fb5b97476495f9b7bab07be820
Patrice <poulin.patrice@gmail.com>
2026-03-26 21:07:41 +0000

    Add PostHog analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import posthog from 'posthog-js';
+import { PostHogProvider } from '@posthog/react';
+
+posthog.init('phc_BOc0SrrBsUTp69xKWe4hGoM3KIEfU2htMh7mK9PXa3', {
+  api_host: 'https://eu.i.posthog.com',
+  defaults: '2026-01-30',
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <PostHogProvider client={posthog}>
+      <App />
+    </PostHogProvider>
   </React.StrictMode>
 );
